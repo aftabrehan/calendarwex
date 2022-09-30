aa5aec958cef6f705b633220220cba94817a25fa
ranaintizar <intizaralirana2>
2022-09-30 12:49:16 +0500

    Add Data from Props

--- pages/stories/sidebar.js
+++ pages/stories/sidebar.js
@@ -1,14 +1,37 @@
 import Stories from 'components/stories'
 import Sidebar from 'components/sidebar'
+import MiniCalendar from 'components/minicalendar/SmallCalendar'
+
+import CelebIcon from '../../src/assets/celebration.svg'
+import CalendarIcon from '../../src/assets/calendar.svg'
 
 const sidebarEmpty = {
   arg: 'SidebarEmpty',
   variant: 'emptySidebar',
+  monthName: 'January',
+  monthData: <MiniCalendar />,
+  upcomingEvents: [
+    {
+      eventHeading: 'Upcoming Events',
+      eventContent: [
+        {
+          eventName: 'No Upcoming Events',
+          // eventTime: '08:00',
+        },
+      ],
+    },
+  ],
 }
 
 const sidebarDefault = {
   arg: 'SidebarDefault',
   variant: 'defaultSidebar',
+  monthName: 'January',
+  monthData: <MiniCalendar />,
+  eventDay: 'Today',
+  eventName: [{ eventName: 'Daily Standup' }],
+  eventTime: new Date().toTimeString(),
+  icon: <CalendarIcon />,
 }
 
 const SidebarStory = () => {
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,23 +1,48 @@
-import SmallCalendar from 'components/minicalendar'
+import Button from 'components/button'
 import stl from './Sidebar.module.scss'
-import Celebration from '../../assets/celebration.svg'
 
-const Sidebar = ({ monthName, monthData, upcomingEvents }) => {
+import AddIcon from '../../assets/addIcon.svg'
+
+const Sidebar = ({
+  arg,
+  variant,
+  icon,
+  monthName,
+  monthData,
+  upcomingEvents,
+}) => {
   return (
     <div className={stl.sidebar}>
       <div className={stl.monthHeader}>
-        <div className={stl.monthName}>January</div>
+        <div className={stl.monthName}>{monthName}</div>
       </div>
-      <SmallCalendar />
-      <div className={stl.upcomingEvents}>
-        <div className={stl.eventsHeading}>Upcoming Events</div>
-        <div className={stl.upcmngEvtCntnt}>
-          <Celebration className={stl.celebIcon} />
-          <div className={stl.emptyEvent}>No Upcoming Events</div>
+      <div>{monthData}</div>
+      {upcomingEvents.map((events, i) => (
+        <div key={i} className={stl.upcomingEvents}>
+          <div className={stl.eventsHeading}>
+            {events.eventHeading}
+            {upcomingEvents[i].eventContent.map((eventCntnt, index) => (
+              <div key={index} className={stl.upcmngEvtCntnt}>
+                <div className={stl.eventName}>{eventCntnt.eventName}</div>
+                <div className={stl.eventName}>
+                  {eventCntnt.eventTime || (
+                    <Button variant="fill" icon={<AddIcon />} />
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   )
 }
 
 export default Sidebar
+
+// <div className={stl.upcomingEvents}>
+// <div className={stl.eventsHeading}>Upcoming Events</div>
+// <div className={stl.upcmngEvtCntnt}>
+//   <div className={stl.eventName}>No Upcoming Events</div>
+// </div>
+// </div>
