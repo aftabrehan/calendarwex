9357a703aa6d424f15ed2105a6a8822438731b82
ranaintizar <intizaralirana2@gmail.com>
2022-10-21 15:27:21 +0500

    Decrease Form animation-duration

--- src/components/sign-up & sign-in/index.js
+++ src/components/sign-up & sign-in/index.js
@@ -3,22 +3,6 @@ import { useEffect, useState } from 'react'
 import SignUp from './signup'
 import SignIn from './signin'
 
-import clsx from 'clsx'
-
-import stl from './SignUp&SignIn.module.scss'
-
-import Image from 'next/image'
-
-import PersonIcon from 'assets/person_filled_FILL1_wght400_GRAD0_opsz20.svg'
-import PassIcon from 'assets/lock_FILL1_wght400_GRAD0_opsz20.svg'
-import FacebookIcon from 'assets/facebook-svgrepo-com.svg'
-import GoogleIcon from 'assets/google-plus-3-logo-svgrepo-com.svg'
-import TwitterIcon from 'assets/twitter-svgrepo-com.svg'
-import MailIcon from 'assets/mail_FILL1_wght400_GRAD0_opsz20.svg'
-import ConfirmPassIcon from 'assets/lock_FILL0_wght400_GRAD0_opsz20.svg'
-import SignUpImage from './images/signup-image.jpg'
-import SignInImage from './images/signin-image.jpg'
-
 const Form = () => {
   const [formType, setFormType] = useState(0)
 
