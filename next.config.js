48015ddad77b6f3a198c1a03ea9c73347a3d200d
aftabrehan <aftabrehan52422@gmail.com>
2022-10-21 08:48:44 +0500

    Set up Firebase Hosting

--- next.config.js
+++ next.config.js
@@ -8,10 +8,7 @@ module.exports = {
   images: { unoptimized: true },
   webpack: config => {
     config.resolve.modules.push(path.resolve('./'))
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
+    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] })
 
     return config
   },
